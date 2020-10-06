7448c79032e77c913f98ce7b910651f4e5dafad6
Artem Shinaruk <artasend@gmail.com>
2020-10-06 22:20:23 +0700

    Init

---
+++ private/tests/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->get('/');
+
+        $this->assertEquals(
+            $this->app->version(),
+            $this->response->getContent()
+        );
+    }
+}
